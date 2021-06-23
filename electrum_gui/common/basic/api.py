bc52d4bc57b11e9cdf1bcbef8cd99d0ed48478dc
taimanhui <taimanhui@protonmail.com>
2021-06-23 15:27:19 +0800

    api: add version V3 and some cleanup
    Signed-off-by: taimanhui <taimanhui@protonmail.com>

--- electrum_gui/common/basic/api.py
+++ electrum_gui/common/basic/api.py
@@ -8,8 +8,12 @@ from electrum_gui.common.basic.functional import json_encoders
 
 @enum.unique
 class Version(enum.IntEnum):
-    V1 = 1
-    V2 = 2
+    V1 = 1  # Legacy
+    V2 = 2  # Unify return values and exceptions
+    V3 = 3  # Single kwarg params as a json str
+
+
+SUPPORTED_VERSIONS = list(Version)
 
 
 @enum.unique
@@ -31,38 +35,56 @@ def api_entry(force_version: int = None):
                     error_msg.update({"err_msg_detail": "%s:%s" % (err.__class__.__name__, err.args)})
                 return error_msg
 
+            pre_run_failure_msg = None
             api_version = kwargs.pop("api_version", Version.V1)
             api_version = force_version or api_version
 
-            if api_version == Version.V1:
+            if api_version == Version.V1:  # Legacy
                 return func(*args, **kwargs)
-            elif api_version == Version.V2:
-                out = {"other_info": ""}
+
+            # Version.V2 onward
+
+            if api_version not in SUPPORTED_VERSIONS:
+                pre_run_failure_msg = "Unsupported API version."
+            elif api_version >= Version.V3:  # Load (and check) params
+                params = kwargs.pop("params", None)
+                # TODO:
+                #  - check len(args) <= 1, only for the main resource id
+                #  - check kwargs is now empty, all the parameters should be put in params
+                if params is not None:
+                    try:
+                        params = json.loads(params)
+                        if not isinstance(params, dict):
+                            raise ValueError
+                        # TODO: add param validation
+                    except ValueError:
+                        pre_run_failure_msg = "Failed to load params."
+                    else:
+                        kwargs["params"] = params
+
+            out = {
+                "status": ResultStatus.FAILED,
+                "api_version": api_version,
+                "other_info": "",
+            }
+            if pre_run_failure_msg is None:
                 try:
                     result = func(*args, **kwargs)
-                    out.update({"status": ResultStatus.SUCCESS, "info": result})
                 except exceptions.OneKeyException as e:
-                    out.update({"status": ResultStatus.FAILED, "err_msg_key": e.key, "other_info": e.other_info})
+                    out.update({"err_msg_key": e.key, "other_info": e.other_info})
                 except Exception as e:
                     out.update(
                         {
-                            "status": ResultStatus.FAILED,
                             "err_msg_key": exceptions.OneKeyException.key,
                             "low_level_error": _filter_params(e),
                         }
                     )
-                out.update({"api_version": api_version})
-                return json.dumps(out, cls=json_encoders.DecimalEncoder)
+                else:
+                    out.update({"status": ResultStatus.SUCCESS, "info": result})
             else:
-                return json.dumps(
-                    {
-                        "status": ResultStatus.FAILED,
-                        "err_msg_key": "msg_unknown_error",
-                        "low_level_error": "unsupported api version",
-                        "api_version": api_version,
-                        "other_info": "",
-                    }
-                )
+                out.update({"err_msg_key": exceptions.OneKeyException.key, "low_level_error": pre_run_failure_msg})
+
+            return json.dumps(out, cls=json_encoders.DecimalEncoder)
 
         return wrapper
 
