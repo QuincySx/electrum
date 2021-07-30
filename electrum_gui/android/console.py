QuincySx/electrum

commit 0c25255947538419f1d828235da235c5b1d8b358

fix: remove lowercase processing of the contract address and fix sol transaction parse bug

electrum_gui/android/console.py
@@ -2414,9 +2414,7 @@ class AndroidCommands(commands.Commands):
         tokens_dict = self._tokens_dict_of_chain.get(chain_code)
 
         if tokens_dict is None:
-            tokens_dict = {
-                i["address"].lower(): i for i in read_json(f"{chain_code}_token_list.json", {}).get("tokens", ())
-            }
+            tokens_dict = {i["address"]: i for i in read_json(f"{chain_code}_token_list.json", {}).get("tokens", ())}
             self._tokens_dict_of_chain[chain_code] = tokens_dict
 
         return tokens_dict
@@ -2440,14 +2438,12 @@ class AndroidCommands(commands.Commands):
         token_dict = self._load_tokens_dict(chain_code)
         top_50_tokens = set(itertools.islice(token_dict.keys(), 50))
         db_token_coins = coin_manager.get_coins_by_chain(chain_code)
-        custom_token_coins = (
-            i for i in db_token_coins if i.token_address and i.token_address.lower() not in top_50_tokens
-        )
+        custom_token_coins = (i for i in db_token_coins if i.token_address not in top_50_tokens)
         custom_token_info_list = [
             {
                 "chain_id": coin_manager.get_chain_info(chain_code).chain_id,
                 "decimals": i.decimals,
-                "address": i.token_address.lower(),
+                "address": i.token_address,
                 "symbol": i.symbol,
                 "name": i.name,
                 "logoURI": i.icon or "",
@@ -2462,7 +2458,7 @@ class AndroidCommands(commands.Commands):
     def add_token(self, symbol, contract_addr, coin=None):
         """
         Add token to eth, for eth/bsc only
-        :param symbol: coin symbol
+        :param symbol: coin symbol only use when not specified by settings
         :param contract_addr: coin address
         :return: raise except if error
         """
@@ -2473,22 +2469,22 @@ class AndroidCommands(commands.Commands):
             coin = self.wallet.coin if self.wallet is not None else "eth"
 
         chain_code = coin_manager.legacy_coin_to_chain_code(coin)
-        chain_info = coin_manager.get_chain_info(chain_code)
-        if chain_info.chain_affinity == "eth":
-            contract_addr = contract_addr.lower()
+        contract_addr = provider_manager.verify_address(chain_code, contract_addr).normalized_address
 
         token_info = self._load_tokens_dict(chain_code).get(contract_addr)
         if token_info is not None:
-            symbol = token_info["symbol"]
+            _symbol = token_info["symbol"]
             decimals = token_info["decimals"]
             name = token_info.get("name")
             icon = token_info.get("logoURI")
         else:
-            symbol, name, decimals = provider_manager.get_token_info_by_address(chain_code, contract_addr)
+            _symbol, name, decimals = provider_manager.get_token_info_by_address(chain_code, contract_addr)
+            if symbol:
+                _symbol = symbol
             icon = None
 
-        coin_manager.add_coin(chain_code, contract_addr, symbol, decimals, name, icon)
-        self.wallet.add_contract_token(symbol, contract_addr)
+        coin_manager.add_coin(chain_code, contract_addr, _symbol, decimals, name, icon)
+        self.wallet.add_contract_token(_symbol, contract_addr)
 
     @api.api_entry()
     def delete_token(self, contract_addr):

electrum_gui/common/coin/daos.py
@@ -43,6 +43,6 @@ def update_coin_info(coin_code: str, name: str = None, icon: str = None):
 def query_coins_by_token_addresses(chain_code: str, token_addresses: List[str]) -> List[CoinInfo]:
     models = CoinModel.select().where(
         CoinModel.chain_code == chain_code,
-        CoinModel.token_address.collate("NOCASE") << token_addresses,
+        CoinModel.token_address << token_addresses,
     )
     return [i.to_dataclass() for i in models]

electrum_gui/common/coin/manager.py
@@ -127,6 +127,7 @@ def add_coin(
     name: str = None,
     icon: str = None,
 ) -> str:
+    token_address = provider_manager.verify_address(chain_code, token_address).normalized_address
     coin_code = _generate_coin_code(chain_code, token_address, symbol)
 
     if coin_code in COINS_DICT:
@@ -195,6 +196,7 @@ def query_coins_by_token_addresses(chain_code: str, token_addresses: List[str]) 
 
 def get_coin_by_token_address(chain_code: str, token_address: str, add_if_missing: bool = False) -> Optional[CoinInfo]:
     coin = None
+    token_address = provider_manager.verify_address(chain_code, token_address).normalized_address
     coins = query_coins_by_token_addresses(chain_code, [token_address])
 
     if coins:

electrum_gui/common/provider/chains/sol/clients/solana.py
@@ -127,6 +127,9 @@ class Solana(ClientInterface, SearchTransactionMixin):
         outputs = []
         for instruction in result["transaction"]["message"]["instructions"]:
             transaction_info = instruction["parsed"]["info"]
+            instruction_type = instruction["parsed"]["type"]
+            if instruction_type != "transfer":
+                continue
             program_id = instruction["programId"]
             from_address = transaction_info["source"]
             to_address = transaction_info["destination"]
@@ -137,7 +140,7 @@ class Solana(ClientInterface, SearchTransactionMixin):
             elif program_id == str(spl_token.SYS_PROGRAM_ID):
                 value = transaction_info["lamports"]
             else:
-                raise Exception("unknown program_id")
+                continue
             inputs.append(TransactionInput(address=from_address, value=value, token_address=token_address))
             outputs.append(TransactionOutput(address=to_address, value=value, token_address=token_address))
         fee = result["meta"]["fee"]

electrum_gui/common/tests/unit/coin/test_manager.py
@@ -29,7 +29,9 @@ class TestCoinManager(TestCase):
 
         cls.coin_btc = Mock(chain_code="btc", code="btc")
         cls.coin_eth = Mock(chain_code="eth", code="eth")
-        cls.coin_eth_usdt = Mock(chain_code="eth", code="eth_usdt", token_address="0x12")
+        cls.coin_eth_usdt = Mock(
+            chain_code="eth", code="eth_usdt", token_address="0x52ce071bd9b1c4b00a0b92d298c512478cad67e7"
+        )
         cls.coin_bsc = Mock(chain_code="bsc", code="bsc")
 
         cls.coin_ont = Mock(chain_code="ont", code="ont")
@@ -45,15 +47,30 @@ class TestCoinManager(TestCase):
 
     def setUp(self) -> None:
         self.coin_db_eth_usdc = data.CoinInfo(
-            code="eth_usdc", chain_code="eth", name="USD Coin", symbol="USDC", decimals=6, token_address="0x11"
+            code="eth_usdc",
+            chain_code="eth",
+            name="USD Coin",
+            symbol="USDC",
+            decimals=6,
+            token_address="0x52ce071bd9b1c4b00a0b92d298c512478cad67e8",
         )
 
         self.coin_db_eth_usdt = data.CoinInfo(
-            code="eth_usdt", chain_code="eth", name="USDT", symbol="USDT", decimals=6, token_address="0x12"
+            code="eth_usdt",
+            chain_code="eth",
+            name="USDT",
+            symbol="USDT",
+            decimals=6,
+            token_address="0x52ce071bd9b1c4b00a0b92d298c512478cad67e7",
         )
 
         self.coin_db_bsc_usdc = data.CoinInfo(
-            code="bsc_usdc", chain_code="bsc", name="USD Coin", symbol="USDC", decimals=6, token_address="0x21"
+            code="bsc_usdc",
+            chain_code="bsc",
+            name="USD Coin",
+            symbol="USDC",
+            decimals=6,
+            token_address="0x52ce071bd9b1c4b00a0b92d298c512478cad67e6",
         )
 
         daos.add_coin(
@@ -156,13 +173,18 @@ class TestCoinManager(TestCase):
     @patch("electrum_gui.common.coin.manager.daos.update_coin_info")
     def test_add_coin(self, fake_update_coin_info, fake_add_coin):
         with self.subTest("Add coin that already exists in the local list"):
-            self.assertEqual("eth_usdt", manager.add_coin("eth", "0x12", "USDT", 6))
+            self.assertEqual(
+                "eth_usdt", manager.add_coin("eth", "0x52ce071bd9b1c4b00a0b92d298c512478cad67e7", "USDT", 6)
+            )
             fake_add_coin.assert_not_called()
             fake_update_coin_info.assert_not_called()
 
         with self.subTest("Add coin that already exists in the db"):
             self.assertEqual(
-                "eth_usdc", manager.add_coin("eth", "0x11", "USDC", 6, name="New USD Coin", icon="new icon")
+                "eth_usdc",
+                manager.add_coin(
+                    "eth", "0x52ce071bd9b1c4b00a0b92d298c512478cad67e8", "USDC", 6, name="New USD Coin", icon="new icon"
+                ),
             )
             fake_add_coin.assert_not_called()
             fake_update_coin_info.assert_called_once_with("eth_usdc", name="New USD Coin", icon="new icon")
@@ -170,13 +192,16 @@ class TestCoinManager(TestCase):
 
         with self.subTest("Add coin with duplicated symbol"):
             self.assertEqual(
-                "eth_usdc_0x13", manager.add_coin("eth", "0x13", "USDC", 6, name="New USD Coin", icon="new icon")
+                "eth_usdc_0x52ce",
+                manager.add_coin(
+                    "eth", "0x52ce071bd9b1c4b00a0b92d298c512478cad67e5", "USDC", 6, name="New USD Coin", icon="new icon"
+                ),
             )
             fake_add_coin.assert_called_once_with(
                 data.CoinInfo(
-                    code="eth_usdc_0x13",
+                    code="eth_usdc_0x52ce",
                     chain_code="eth",
-                    token_address="0x13",
+                    token_address="0x52ce071bd9b1c4b00a0b92d298c512478cad67e5",
                     symbol="USDC",
                     decimals=6,
                     name="New USD Coin",
@@ -187,13 +212,13 @@ class TestCoinManager(TestCase):
             fake_add_coin.reset_mock()
 
         with self.subTest("Add coin in most cases"):
-            self.assertEqual("eth_cc", manager.add_coin("eth", "0x13", "CC", 18))
+            self.assertEqual("eth_cc", manager.add_coin("eth", "0x52ce071bd9b1c4b00a0b92d298c512478cad67e5", "CC", 18))
 
             fake_add_coin.assert_called_once_with(
                 data.CoinInfo(
                     code="eth_cc",
                     chain_code="eth",
-                    token_address="0x13",
+                    token_address="0x52ce071bd9b1c4b00a0b92d298c512478cad67e5",
                     symbol="CC",
                     decimals=18,
                     name="CC",
@@ -206,18 +231,33 @@ class TestCoinManager(TestCase):
     def test_query_coins_by_token_addresses(self):
         self.assertEqual(
             [self.coin_eth_usdt, self.coin_db_eth_usdc],
-            manager.query_coins_by_token_addresses("eth", ["0x11", "0x12", "0x13"]),
+            manager.query_coins_by_token_addresses(
+                "eth",
+                [
+                    "0x52ce071bd9b1c4b00a0b92d298c512478cad67e8",
+                    "0x52ce071bd9b1c4b00a0b92d298c512478cad67e7",
+                    "0x52ce071bd9b1c4b00a0b92d298c512478cad67e5",
+                ],
+            ),
         )
-        self.assertEqual([self.coin_db_bsc_usdc], manager.query_coins_by_token_addresses("bsc", ["0x21"]))
+        self.assertEqual(
+            [self.coin_db_bsc_usdc],
+            manager.query_coins_by_token_addresses("bsc", ["0x52ce071bd9b1c4b00a0b92d298c512478cad67e6"]),
+        )
         self.assertEqual([self.coin_ong], manager.query_coins_by_token_addresses("ont", ["0x31"]))
 
     def test_get_coin_by_token_address(self):
         with self.subTest("get coin by token address as expected"):
-            self.assertEqual(self.coin_db_eth_usdc, manager.get_coin_by_token_address("eth", "0x11"))
+            self.assertEqual(
+                self.coin_db_eth_usdc,
+                manager.get_coin_by_token_address("eth", "0x52ce071bd9b1c4b00a0b92d298c512478cad67e8"),
+            )
 
         with self.subTest("get coin by token address but nothing found"):
-            with self.assertRaisesRegex(exceptions.CoinNotFoundByTokenAddress, "0x13"):
-                manager.get_coin_by_token_address("eth", "0x13")
+            with self.assertRaisesRegex(
+                exceptions.CoinNotFoundByTokenAddress, "0x52ce071bd9b1c4b00a0b92d298c512478cad67e5"
+            ):
+                manager.get_coin_by_token_address("eth", "0x52ce071bd9b1c4b00a0b92d298c512478cad67e5")
 
         with self.subTest("automatically add token address"):
             with patch(
@@ -228,13 +268,15 @@ class TestCoinManager(TestCase):
                     data.CoinInfo(
                         code="eth_cc",
                         chain_code="eth",
-                        token_address="0x13",
+                        token_address="0x52ce071bd9b1c4b00a0b92d298c512478cad67e5",
                         symbol="CC",
                         decimals=18,
                         name="Chain Coin",
                         icon=None,
                     ),
-                    manager.get_coin_by_token_address("eth", "0x13", add_if_missing=True),
+                    manager.get_coin_by_token_address(
+                        "eth", "0x52ce071bd9b1c4b00a0b92d298c512478cad67e5", add_if_missing=True
+                    ),
                 )
 
     @patch("electrum_gui.common.coin.manager.settings")

commit 5c86964c3b4ce2ca2843fbd18f17892e3e611321

console: fix get_all_customer_token_info
Fixes: 0c2525594 ("fix: remove lowercase processing of the contract
address and fix sol transaction parse bug")
Was in PR #867

Signed-off-by: taimanhui <taimanhui@protonmail.com>

electrum_gui/android/console.py
@@ -2438,7 +2438,7 @@ class AndroidCommands(commands.Commands):
         token_dict = self._load_tokens_dict(chain_code)
         top_50_tokens = set(itertools.islice(token_dict.keys(), 50))
         db_token_coins = coin_manager.get_coins_by_chain(chain_code)
-        custom_token_coins = (i for i in db_token_coins if i.token_address not in top_50_tokens)
+        custom_token_coins = (i for i in db_token_coins if i.token_address and i.token_address not in top_50_tokens)
         custom_token_info_list = [
             {
                 "chain_id": coin_manager.get_chain_info(chain_code).chain_id,